ae6941079434e7d2948a1d262999825c2e5d3331
Joshua Alphonse <joshalphonse@gmail.com>
2019-04-13 16:59:48 -0400

    Yeer2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import Dashboard from './containers/Dashboard'
+import React, { Component } from "react";
+import Dashboard from "./containers/Dashboard";
 // import './stylesheets/main.scss';
 // import MyNavbar from './components/MyNavbar';
 // import {Container} from 'react-bulma-components';
+import { Router, Switch, withRouter } from "react-router-dom";
+import DrugShow from "./components/DrugShow";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-      <Dashboard/>
-     </div>
+        <Switch>
+          <Router exact path="/meds" component={Dashboard} />
+          <Router exact path="/" component={Dashboard} />
+        </Switch>
+      </div>
     );
   }
 }
---
+++ src/components/DrugShow.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+const DrugShow = props => {
+  return (
+    <div>
+      <div>IM A DIV</div>
+    </div>
+  );
+};
+export default DrugShow;
